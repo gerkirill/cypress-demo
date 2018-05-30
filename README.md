e26f10ffaecf9a55dda0de6638c523751c3ab206
kirill <ger.kirill@gmail.com>
2018-05-30 17:05:47 +0300

    adds readme

---
+++ README.md
@@ -0,0 +1,9 @@
+## Cypress demo
+
+Cypress website is https://www.cypress.io/
+
+Open https://gerkirill.github.io/cypress-demo/ to see the application under the test.
+
+Run `npx cypress open` to start cypress UI.
+
+Run `npm test` to start tests in console.
--- cypress/integration/timer.spec.js
+++ cypress/integration/timer.spec.js
@@ -1,12 +1,19 @@
 context('Timer', () => {
   beforeEach(() => {
-    cy.visit('https://codepen.io/gerkirill/full/BVyLgK/')
+    cy.visit('https://gerkirill.github.io/cypress-demo/')
   })
 
-
-  it('start', () => {
+  it('should count 3 seconds properly', () => {
     cy.get('.startStop')
-      .should('have.text', 'Start')
+      .should('have.value', 'Start')
+      .click()
+      .should('have.value', 'Stop')
+    ;
+    cy.wait(3000);
+    cy.get('.tablo')
+      .should('have.text', '00:00:03')
+    ;
+    cy.get('.startStop')
       .click()
     ;
   })
